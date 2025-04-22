f6c132ce68349d8222608275a0c50ceab6605f0b
sairaj8-dev <sairajshendre288@gmail.com>
2025-04-23 00:25:37 +0530

    Add files via upload

---
+++ exp3.asm
@@ -0,0 +1,27 @@
+SJMP 30H
+	ORG 30H
+		
+		MOV R2 ,#04
+		
+		OUTER:
+		MOV R1 ,#04
+		MOV R0,#30H
+		
+		INNER:
+		MOV A,@R0
+		INC R0
+		MOV B,@R0
+		CJNE A,B,CHECK
+		SJMP NEXT
+		
+		CHECK:
+		JC NEXT
+		MOV @R0,A
+		DEC R0
+		MOV @R0,B
+		INC R0
+		
+		NEXT:
+		DJNZ R1,INNER
+		DJNZ R2,OUTER
+		END
